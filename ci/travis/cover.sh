f067aecf4e5e06d16bcffe131748ead5e789e68a
kngwyu <yuji.kngw.80s.revive@gmail.com>
2020-06-26 16:36:41 +0900

    Use RUSTC_BOOTSTRAP=1 for coverage

--- ci/travis/cover.sh
+++ ci/travis/cover.sh
@@ -2,15 +2,10 @@
 
 set -ex
 
-### PyPy does not run the test suite ###########################################
-
-if [[ $FEATURES == *"pypy"* ]]; then
-  exit 0
-fi
-
 ### Run grcov ##################################################################
 # export env vars and re-run tests
 export CARGO_INCREMENTAL=0
+export RUSTC_BOOTSTLAP=1
 export RUSTFLAGS="-Zpanic_abort_tests -Zprofile -Cpanic=abort -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off"
 export RUSTDOCFLAGS="-Cpanic=abort"
 cargo test --features "$FEATURES num-bigint num-complex"
@@ -18,4 +13,3 @@ cargo test --features "$FEATURES num-bigint num-complex"
 zip -0 ccov.zip `find . \( -name "pyo3*.gc*" \) -print`;
 ./grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o lcov.info;
 bash <(curl -s https://codecov.io/bash) -f lcov.info;
-
